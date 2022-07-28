0fea673bec5a25907ac2781ac049f1424e8b5f4b
nNipsx <86789668+nNipsx-Sec@users.noreply.github.com>
2022-07-28 17:04:54 +0700

    update test backend

--- tests/test_backend_netwitness.py
+++ tests/test_backend_netwitness.py
@@ -1,13 +1,19 @@
+from io import BytesIO
+from zipfile import ZipFile
 import pytest
 from sigma.backends.netwitness import NetwitnessBackend, NetwitnessEPLBackend
 from sigma.collection import SigmaCollection
 
 @pytest.fixture
-def qradar_backend():
+def netwitness_backend():
     return NetwitnessBackend()
+@pytest.fixture    
+def netwitness_epl_backend():
+    return NetwitnessEPLBackend()
 
-def test_qradar_in_expression(qradar_backend : NetwitnessBackend):
-    assert qradar_backend.convert(
+# Testing for netwitness backend
+def test_netwitness_in_expression(netwitness_backend : NetwitnessBackend):
+    assert netwitness_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
@@ -22,10 +28,10 @@ def test_qradar_in_expression(qradar_backend : NetwitnessBackend):
                         - valueC*
                 condition: sel
         """),
-    ) == ['SELECT UTF8(payload) as search_payload from events where "fieldA" ILIKE ENUMERATION(\'valueA\',\'valueB\',\'valueC%\')']
+    ) == ["fieldA='valueA'  ||  fieldA='valueB'  ||  fieldA begins 'valueC'"]
 
-def test_qradar_regex_query(qradar_backend : NetwitnessBackend):
-    assert qradar_backend.convert(
+def test_netwitness_regex_query(netwitness_backend : NetwitnessBackend):
+    assert netwitness_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
@@ -39,10 +45,10 @@ def test_qradar_regex_query(qradar_backend : NetwitnessBackend):
                     fieldC: bar
                 condition: sel
         """)
-    ) == ['SELECT UTF8(payload) as search_payload from events where "fieldA" IMATCHES \'foo.*bar\' AND "fieldB"=\'foo\' AND "fieldC"=\'bar\'']
+    ) == ["fieldA regex 'foo.*bar'  &&  fieldB='foo'  &&  fieldC='bar'"]
 
-def test_qradar_single_regex_query(qradar_backend : NetwitnessBackend):
-    assert qradar_backend.convert(
+def test_netwitness_single_regex_query(netwitness_backend : NetwitnessBackend):
+    assert netwitness_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
@@ -54,10 +60,10 @@ def test_qradar_single_regex_query(qradar_backend : NetwitnessBackend):
                     fieldA|re: foo.*bar
                 condition: sel
         """)
-    ) == ['SELECT UTF8(payload) as search_payload from events where "fieldA" IMATCHES \'foo.*bar\'']
+    ) == ["fieldA regex 'foo.*bar'"]
 
-def test_qradar_cidr_query(qradar_backend : NetwitnessBackend):
-    assert qradar_backend.convert(
+def test_netwitness_cidr_query(netwitness_backend : NetwitnessBackend):
+    assert netwitness_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
@@ -71,10 +77,10 @@ def test_qradar_cidr_query(qradar_backend : NetwitnessBackend):
                     fieldC: bar
                 condition: sel
         """)
-    ) == ['SELECT UTF8(payload) as search_payload from events where INCIDR(\'192.168.0.0/16\', "fieldA") AND "fieldB"=\'foo\' AND "fieldC"=\'bar\'']
+    ) ==["fieldA = '192.168.0.0/16'  &&  fieldB='foo'  &&  fieldC='bar'"]
 
 
-def test_qradar_default_output(qradar_backend : NetwitnessBackend):
+def test_netwitness_default_output(netwitness_backend : NetwitnessBackend):
     rules = """
 title: Test 1
 status: test
@@ -99,57 +105,105 @@ detection:
         fieldB: bar
     condition: sel
     """
-    assert qradar_backend.convert(SigmaCollection.from_yaml(rules), ) == [
-        """SELECT UTF8(payload) as search_payload from events where "fieldA" IMATCHES 'foo.*bar' AND "fieldB"='foo' AND "fieldC"='bar'""",
-        """SELECT UTF8(payload) as search_payload from events where "fieldA"='foo' AND "fieldB"='bar'"""
-    ]
+    assert netwitness_backend.convert(SigmaCollection.from_yaml(rules), ) == ["fieldA regex 'foo.*bar'  &&  fieldB='foo'  &&  fieldC='bar'", "fieldA='foo'  &&  fieldB='bar'"]
 
-def test_qradar_extension_output(qradar_backend : NetwitnessBackend):
-    rules = """
-title: Test 1
-status: test
-logsource:
-    category: test_category
-    product: test_product
-detection:
-    sel:
-        fieldA|re: foo.*bar
-        fieldB: foo
-        fieldC: bar
-    condition: sel
----
-title: Test 2
-status: test
-logsource:
-    category: test_category
-    product: test_product
-detection:
-    sel:
-        fieldA: foo
-        fieldB: bar
-    condition: sel
-    """
-    result = qradar_backend.convert(SigmaCollection.from_yaml(rules), "extension")
-    assert isinstance(result, bytes)
 
-    zip_io = BytesIO(result)
-    zip_file = ZipFile(zip_io)
-    xml = str(zip_file.read("sigmaQradar.xml"), "utf-8")
-    assert xml.startswith('<?xml version="1.0" encoding="UTF-8"?>')
 
-qradar_backends = NetwitnessBackend()
-
-a = qradar_backends.convert(
+# Testing for netwitness epl backend
+def test_netwitness_epl_in_expression(netwitness_epl_backend : NetwitnessEPLBackend):
+    assert netwitness_epl_backend.convert(
         SigmaCollection.from_yaml("""
             title: Test
             status: test
             logsource:
-                category: process_creation
-                product: windows
+                category: test_category
+                product: test_product
             detection:
                 sel:
-                    sha1: val1
-                    Image: val2
+                    fieldA:
+                        - valueA
+                        - valueB
+                        - valueC*
                 condition: sel
-        """),"default")
-print(a)
+        """),
+    ) == ["module Test;\n@Name('Test')\n@Description('Test\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA = 'valueA'  OR  fieldA = 'valueB'  OR  fieldA like 'valueC%'\n);"]
+
+def test_netwitness_epl_regex_query(netwitness_epl_backend : NetwitnessEPLBackend):
+    assert netwitness_epl_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA|re: foo.*bar
+                    fieldB: foo
+                    fieldC: bar
+                condition: sel
+        """)
+    ) == ["module Test;\n@Name('Test')\n@Description('Test\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA REGEXP 'foo.*bar'  AND  fieldB = 'foo'  AND  fieldC = 'bar'\n);"]
+
+def test_netwitness_epl_single_regex_query(netwitness_epl_backend : NetwitnessEPLBackend):
+    assert netwitness_epl_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA|re: foo.*bar
+                condition: sel
+        """)
+    ) == ["module Test;\n@Name('Test')\n@Description('Test\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA REGEXP 'foo.*bar'\n);"]
+
+def test_netwitness_epl_cidr_query(netwitness_epl_backend : NetwitnessEPLBackend):
+    assert netwitness_epl_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA|cidr: 192.168.0.0/16
+                    fieldB: foo
+                    fieldC: bar
+                condition: sel
+        """)
+    ) ==["module Test;\n@Name('Test')\n@Description('Test\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA = '192.168.0.0/16'  AND  fieldB = 'foo'  AND  fieldC = 'bar'\n);"]
+
+
+def test_netwitness_epl_default_output(netwitness_epl_backend : NetwitnessEPLBackend):
+    rules = """
+title: Test 1
+status: test
+logsource:
+    category: test_category
+    product: test_product
+detection:
+    sel:
+        fieldA|re: foo.*bar
+        fieldB: foo
+        fieldC: bar
+    condition: sel
+---
+title: Test 2
+status: test
+logsource:
+    category: test_category
+    product: test_product
+detection:
+    sel:
+        fieldA: foo
+        fieldB: bar
+    condition: sel
+    """
+    assert netwitness_epl_backend.convert(SigmaCollection.from_yaml(rules), ) == ["module Test_1;\n@Name('Test_1')\n@Description('Test 1\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA REGEXP 'foo.*bar'  AND  fieldB = 'foo'  AND  fieldC = 'bar'\n);", "module Test_2;\n@Name('Test_2')\n@Description('Test 2\nReferences:\n- ')\n@RSAAlert(oneInSeconds=0) \nSELECT * FROM Event(\nfieldA = 'foo'  AND  fieldB = 'bar'\n);"]
+
+
+
